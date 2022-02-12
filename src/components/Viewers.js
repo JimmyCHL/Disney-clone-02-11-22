05e4ac555dd75af438b7f8106457703c31f4566d
Jimmy Lo <83715466+JimmyCHL@users.noreply.github.com>
2022-02-11 23:54:03 -0500

    add video

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,41 +1,65 @@
 import React from "react";
 import styled from "styled-components";
-
+import ReactPlayer from "react-player/file";
 const Viewers = () => {
   return (
     <Container>
       <Wrap>
         <img src="/images/viewers-disney.png" alt="" />
-        <video autoPlay loop>
-          <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
-        </video>
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          playing={true}
+          loop={true}
+          muted={true}
+          url="/videos/1564674844-disney.mp4"
+        />
       </Wrap>
       <Wrap>
         <img src="/images/viewers-pixar.png" alt="" />
-        <video autoPlay loop>
-          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
-        </video>
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          playing={true}
+          loop={true}
+          muted={true}
+          url="/videos/1564676714-pixar.mp4"
+        />
       </Wrap>
       <Wrap>
         <img src="/images/viewers-marvel.png" alt="" />
-        <video autoPlay loop>
-          <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
-        </video>
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          playing={true}
+          loop={true}
+          muted={true}
+          url="/videos/1564676115-marvel.mp4"
+        />
       </Wrap>
       <Wrap>
         <img src="/images/viewers-starwars.png" alt="" />
-        <video autoPlay loop>
-          <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
-        </video>
+
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          playing={true}
+          muted={true}
+          loop={true}
+          url="/videos/1608229455-star-wars.mp4"
+        />
       </Wrap>
       <Wrap>
         <img src="/images/viewers-national.png" alt="" />
-        <video autoPlay loop>
-          <source
-            src="/videos/1564676296-national-geographic.mp4"
-            type="video/mp4"
-          />
-        </video>
+
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          playing={true}
+          loop={true}
+          muted={true}
+          url="/videos/1564676296-national-geographic.mp4"
+        />
       </Wrap>
     </Container>
   );
